60fdadffaf1e942c95a3dd2e90c7f49d9a45ce9b
Connor Masterson <ccmaster@ucsc.edu>
2021-12-22 13:37:35 -0800

    fixed point addtion

--- src/main/scala/PippengerModel/Point.scala
+++ src/main/scala/PippengerModel/Point.scala
@@ -1,5 +1,7 @@
 package PippengerModel
 
+import Util._
+
 /**
  * This class represents a point on an elliptic curve in a finite field.
  * It takes just two arguments, an X and Y coordinate. There are a number
@@ -11,9 +13,9 @@ class Point(coord_x: Int, coord_y: Int, ec: EllipticCurve) {
   val curve: EllipticCurve = ec
 
   def double() = {
-    val l = (3 * this.x * this.x + this.curve.a) / (2 * this.y)
-    val new_x = ((l * l)  - this.x - this.x) //% this.curve.p
-    val new_y = ((l * (this.x - new_x)) - this.y) //% this.curve.p
+    val l = (3 * this.x * this.x + this.curve.a) * Util.mod_inverse(2 * this.y, this.curve.p)
+    val new_x = ((l * l)  - this.x - this.x) % this.curve.p
+    val new_y = ((l * (this.x - new_x)) - this.y) % this.curve.p
     new Point(new_x, new_y, this.curve)
   }
 
@@ -21,20 +23,29 @@ class Point(coord_x: Int, coord_y: Int, ec: EllipticCurve) {
     if (this == that) {
       return this.double()
     }
-    val l = (that.y - this.y) / (that.x - this.x)
-    val new_x = ((l * l)  - this.x - that.x) //% this.curve.p
-    val new_y = ((l * (this.x - new_x)) - this.y) //% this.curve.p
+    val l = (that.y - this.y) * Util.mod_inverse(that.x - this.x, this.curve.p)
+    var new_x = ((l * l)  - this.x - that.x) % this.curve.p
+    if (new_x < 0) {
+      new_x = new_x + this.curve.p
+    }
+    var new_y = ((l * (this.x - new_x)) - this.y) % this.curve.p
+    if (new_y < 0) {
+      new_y = new_y + this.curve.p
+    }
     new Point(new_x, new_y, this.curve)
   }
 
   def unary_- = new Point(this.x, this.y * -1, this.curve)
 
   def *(n: Int): Point = {
+    if (n == 0) {
+      return new Point(0, 0, this.curve)
+    }
     def mult_helper(orig: Point, acc: Point, n: Int): Point = {
       if (n == 1) {
         return acc
       } else {
-        val new_total = orig + acc
+        val new_total = acc + orig
         mult_helper(orig, new_total, n-1)
       }
     }
@@ -49,4 +60,3 @@ class Point(coord_x: Int, coord_y: Int, ec: EllipticCurve) {
     println(s"I am the point (${this.x}, ${this.y})")
   }
 }
-
--- src/main/scala/PippengerModel/Util.scala
+++ src/main/scala/PippengerModel/Util.scala
@@ -11,6 +11,7 @@ object Util {
     }
   }
 
+  // finds the modular inverse of a mod(p)
   def mod_inverse(a: Int, p: Int): Int = {
     def mod_inv_recurse(a: Int, p: Int, n: Int): Int = {
       val r = (a * n) % p
@@ -24,5 +25,21 @@ object Util {
       return p - mod_inverse(-a, p)
     }
     mod_inv_recurse(a, p, 0)
+  }
+
+  // this performs bitwise multiplication. There is no need to actually
+  // do multiplication this was in pure Scala, but it is a model of how
+  // hardware could do this multiplication efficiently. This is just here
+  // for personal reference.
+  def bitwisemultiplication_Model(a: Int, b: Int): Int = {
+    var result = 0
+    for (i <- 0 until 32) {
+      val operand = b << i
+      val mask = a >> i
+      if ((mask & 1) == 1) {
+        result += operand
+      }
+    }
+    result
   }
 }
