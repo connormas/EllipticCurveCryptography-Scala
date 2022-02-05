3124290f07cc1021e47442ce41642ba7e5f75029
Connor Masterson <ccmaster@ucsc.edu>
2022-02-05 11:56:39 -0800

    moved zksnarkMSM_model to Util.scala

--- src/main/scala/PippengerModel/PippengerModel.scala
+++ src/main/scala/PippengerModel/PippengerModel.scala
@@ -4,35 +4,17 @@ import Util._
 
 object PippengerModel extends App {
 
-  /**
-   * This mimics what the MSM part of zkSNARK does. It takes in two vectors.
-   * One vectors of scalars, and another vector of points. It multiplies each
-   * scalar by its corresponding point and them sums up all the points.
-   * (right now, we are just doing this with two int vectors to verify
-   * that it is working properly.)
-   */
-  def zksnarkMSM_model(g: List[Int], e: List[Int]): Int = {
-    assert(g.length == e.length, "vectors should be the same length")
-    (g.map(x => x * e(g.indexOf(x)))).sum
-    // multipy corresponding indexes of arrays and sum them up
-  }
-
   // print out the first 15 points on this elliptic curve
   val p1707 = new EllipticCurve(0, 7, 17)
   val cx = new Point(15, 13, p1707)
-  val p2 = new Point(5, 8, p1707)
-  val cx1 = new Point(3, 0, p1707)
-  val p21 = new Point(6, 11, p1707)
-  val sum = cx + p2
-  val sum1 = cx1 + p21
-  //sum.print()
-  //sum1.print()
 
+  println("Print out first 25 EC Points. (They will repeat.)")
   for (n <- 0 until 25) {
     val pmul = cx * n
     pmul.print()
   }
 
-  //val pmul = cx * 18
-  //pmul.print()
+  println("Perform dot product of List(1,2,3,4,5) with itself and sum elements")
+  val l = List(1,2,3,4,5)
+  println(zksnarkMSM_model(l, l))
 }
